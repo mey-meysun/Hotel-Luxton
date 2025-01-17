a0444415c885fa74d26c29b1871b3d9d818e5635
Feillyn <belindanabilah6@gmail.com>
2025-01-17 11:01:42 +0700

    ubah home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,6 @@
 <?php
-<<<<<<< HEAD
 echo "hello bil";
 echo "silit dobolllllllll";
+echo "paraah";
 echo "paraah"
-=======
-echo "hello bil"
-echo "jorok"
->>>>>>> b37d7d14d1f0b77f052dc4d799067c00d7fce630
 ?>
