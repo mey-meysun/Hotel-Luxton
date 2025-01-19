c6a0de526a033affa8ba27f2bff6457dda389613
mey <153332715+mey-meysun@users.noreply.github.com>
2025-01-19 13:25:07 +0700

    memperbaiki file model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    use HasFactory;
     protected $fillable = ['nama', 'email', 'username', 'password', 'hp', 'role'];
 }
