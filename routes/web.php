82b36af2ef3a808741c45c6f17c4dc9863512025
bilanur <sabilabae924@gmail.com>
2025-01-20 16:53:16 -0800

    tambah login

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,14 @@ Route::get('/home', function () {
     return view('home');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
