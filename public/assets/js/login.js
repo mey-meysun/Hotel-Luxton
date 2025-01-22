666900d2249b499ec912ed3f9d39dd0391024b13
bilanur <sabilabae924@gmail.com>
2025-01-22 00:21:14 -0800

    tambah style regis

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,19 +1,19 @@
 // Fungsi untuk toggle password visibility
-function togglePassword() {
-    const passwordInput = document.getElementById("password");
-    const toggleIcon = document.getElementById("toggle-password");
+// function togglePassword() {
+//     const passwordInput = document.getElementById("password");
+//     const toggleIcon = document.getElementById("toggle-password");
   
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-      toggleIcon.classList.remove("fa-eye");
-      toggleIcon.classList.add("fa-eye-slash");
-    } else {
-      passwordInput.type = "password";
-      toggleIcon.classList.remove("fa-eye-slash");
-      toggleIcon.classList.add("fa-eye");
-    }
-  }
+//     if (passwordInput.type === "password") {
+//       passwordInput.type = "text";
+//       toggleIcon.classList.remove("fa-eye");
+//       toggleIcon.classList.add("fa-eye-slash");
+//     } else {
+//       passwordInput.type = "password";
+//       toggleIcon.classList.remove("fa-eye-slash");
+//       toggleIcon.classList.add("fa-eye");
+//     }
+//   }
   
-  // Menambahkan event listener pada ikon untuk toggle password visibility
-  document.getElementById("toggle-password").addEventListener("click", togglePassword);
+//   // Menambahkan event listener pada ikon untuk toggle password visibility
+//   document.getElementById("toggle-password").addEventListener("click", togglePassword);
   
