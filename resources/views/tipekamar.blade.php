78a24c9635c3dc857c4c96b676a5e360c51944d4
BoyaJvv <boyarizky786@gmail.com>
2025-01-24 08:28:16 +0700

    tambahkan foto

--- resources/views/tipekamar.blade.php
+++ resources/views/tipekamar.blade.php
@@ -15,7 +15,7 @@
     <section id="superior">
         <div class="card border-0" style="display: flex; align-items: center; width: 100rem; margin: 0 1rem; ">
             <div class="login-box p-4 shadow">
-              <img src="{{ route('room1.wedp') }}" class=" card-img-top rounded float-start" style="width: 250px; height: auto" alt="">
+              <img src="" class=" card-img-top rounded float-start" style="width: 250px; height: auto" alt="">
                <!-- Product details-->
                <div class="card-body p-4">
                 <p class="text-center">Kamar 1<b></b></p>
