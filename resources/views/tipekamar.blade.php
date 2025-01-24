75424937694e222c073116f3e6ac930b3cdbd3b2
BoyaJvv <boyarizky786@gmail.com>
2025-01-24 08:36:18 +0700

    mengubah border

--- resources/views/tipekamar.blade.php
+++ resources/views/tipekamar.blade.php
@@ -13,7 +13,7 @@
     </section>
 
     <section id="superior">
-        <div class="card border-0" style="display: flex; align-items: center; width: 100rem; margin: 0 1rem; ">
+        <div class="card border-0 rounded-3" style="display: flex; align-items: center; width: 100rem; margin: 0 1rem;">
             <div class="login-box p-4 shadow">
               <img src="{{ asset('assets/images/room1.webp') }}"" class="card-img-top rounded float-start" style="width: 250px; height: auto" alt="Superior Room">
                <!-- Product details-->
