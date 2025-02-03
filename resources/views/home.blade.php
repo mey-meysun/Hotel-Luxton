9dae569a32d181f9a6c53d6bfaca71f2e84aceb7
jannatii78 <jannatiinuril@gmail.com>
2025-02-03 10:14:40 +0700

    ubah

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -139,7 +139,7 @@
                     <div class="container pt-0 pb-5">
                         <div class="row justify-content-center text-center">
                             <div class="col-lg-6 section-heading" data-aos="fade-up">
-                                <h3 class="text-center">Hotel Amenities</h3>
+                                <h3 class="text-center">Fasilitas Hotel</h3>
                             </div>
                         </div>
                     </div>
@@ -233,47 +233,6 @@
                             </div>
                         </div>
 
-                    </div>
-                </div>
-            </div>
-
-            <div class="untree_co--site-section">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-4 section-heading" data-aos="fade-up">
-                            <h3 class="text-left">News &amp; Events</h3>
-                            <div class="w-75">
-                                <p>A hotel is an establishment that provides paid lodging on a short-term basis.</p>
-                            </div>
-                            <p><a href="#" class="readmore">All Posts</a></p>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="post-entry" data-aos="fade-up" data-aos-delay="100">
-                                <a href="#" class="thumb"><img src="images/img_1.jpg" alt="Image"
-                                        class="img-fluid"></a>
-                                <div class="post-entry-contents">
-                                    <h3><a href="#">How algorithms shape our world</a></h3>
-                                    <div class="date">December 20, 2019 &bullet; by <a href="#">Joefrey
-                                            Mahusay</a></div>
-                                    <p>A hotel is an establishment that provides paid lodging on a short-term basis.</p>
-                                    <p><a href="#" class="readmore">Read more</a></p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-4">
-                            <div class="post-entry" data-aos="fade-up" data-aos-delay="200">
-                                <a href="#" class="thumb"><img src="{{ asset('assets/images/img_3.jpg') }}"
-                                        alt="Image" class="img-fluid"></a>
-                                <div class="post-entry-contents">
-                                    <h3><a href="#">How algorithms shape our world</a></h3>
-                                    <div class="date">December 20, 2019 &bullet; by <a href="#">Joefrey
-                                            Mahusay</a></div>
-                                    <p>A hotel is an establishment that provides paid lodging on a short-term basis.</p>
-                                    <p><a href="#" class="readmore">Read more</a></p>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
