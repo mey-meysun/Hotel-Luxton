182e1c67330ab2108448c9c2ff43a19db0607b3c
jannatii78 <jannatiinuril@gmail.com>
2025-02-14 11:13:48 +0700

    ubah

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,6 @@ Route::get('/fasilitas', function () {
 Route::get('/tipekamar', function () {
     return view('tipekamar');
 });
-Route::get('/pesan-kamar', function () {
-    return view('pesan-kamar');
-});
 
 // untuk yg sudah login
 Route::middleware('auth')->group(function () {
