76ec1728b923f0fc5f073c9c28ecab12b3803a9a
jannatii78 <jannatiinuril@gmail.com>
2025-03-12 01:16:16 +0700

    about

--- resources/views/tentangkami.blade.php
+++ resources/views/tentangkami.blade.php
@@ -9,25 +9,24 @@
         
         <div class="container text-center mt-5 pt-4">
             <h2>Tentang Luxton</h2>
-            <p>Kemewahan, Kenyamanan, & Keindahan dalam Satu Tempat</p>
-        
-            <h3>Sejarah Luxton</h3>
-            <p>Luxton telah menjadi simbol kemewahan dan keramahan sejak berdiri pada [tahun]. Dibangun dengan standar internasional, kami memberikan pengalaman terbaik bagi setiap tamu.</p>
-        
-            <img src="{{ asset('img/hotel-luxton.jpg') }}" alt="Hotel Luxton" class="img-fluid rounded my-3">
-        
-            <h3>Keunggulan Kami</h3>
+            <p>Luxton adalah sebuah hotel yang mengutamakan kenyamanan dan layanan berkualitas bagi para tamunya. Dengan desain modern dan fasilitas lengkap, Luxton menawarkan pengalaman menginap yang mewah dan menyenangkan, cocok untuk wisatawan maupun pebisnis.</p>
+            <p>Hotel ini menyediakan berbagai tipe kamar yang dirancang dengan elegan, dilengkapi dengan fasilitas seperti tempat tidur yang nyaman, akses Wi-Fi cepat, TV layar datar, dan kamar mandi modern. Selain itu, Luxton juga memiliki restoran dengan pilihan menu lezat, ruang pertemuan untuk kebutuhan bisnis atau acara, serta area rekreasi seperti kolam renang dan pusat kebugaran.</p>
+            <p>Dengan pelayanan yang ramah dan profesional, Luxton berkomitmen untuk memberikan pengalaman menginap yang tak terlupakan bagi setiap tamunya.</p>
+            
+            <h3 class="mt-5">Fasilitas Unggulan Luxton</h3>
             <div class="row">
                 <div class="col-md-4">
-                    <p>Infinity Pool</p>
+                    <img src="{{ asset('assets/images/ruangmetting.jpg') }}" class="img-fluid rounded" alt="Infinity Pool">
+                    <p>Ruang Meeting</p>
                 </div>
                 <div class="col-md-4">
-                    <p>Gourmet Dining</p>
+                    <img src="{{ asset('assets/images/restoran.jpg') }}" class="img-fluid rounded" alt="Gourmet Dining">
+                    <p>Restoran</p>
                 </div>
                 <div class="col-md-4">
-                    <p>Spa & Wellness</p>
+                    <img src="{{ asset('assets/images/fitness.jpg') }}" class="img-fluid rounded" alt="Spa & Wellness">
+                    <p>Fitnes & Gym</p>
                 </div>
             </div>
         </div>
-            
 </x-layout>
