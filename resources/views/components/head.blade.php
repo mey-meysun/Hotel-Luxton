631150e836b9188648b29de32cc89a8843b0c9dc
bilanur <sabilabae924@gmail.com>
2025-03-14 06:51:57 +0700

    bbsb

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -17,6 +17,7 @@
       <link href="{{ asset('assets/css/vendor/bootstrap.css') }}" rel="stylesheet">
       <link href="{{ asset('assets/css/kamar.css') }}" rel="stylesheet">
       <link href="{{ asset('assets/css/kamar1.css') }}" rel="stylesheet">
+      <link href="{{ asset('assets/css/map.css') }}" rel="stylesheet">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
